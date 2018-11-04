9bcc015aa43166a2f749708abe27e4c7cd3edee5
sashabogdan <sashabogdan@ukr.net>
2018-11-04 11:57:14 +0200

    -

--- src/main/java/com/repository/SupportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.domain.Support;
-
-public interface SupportRepository extends JpaRepository<Support, Integer> {
-
-}
